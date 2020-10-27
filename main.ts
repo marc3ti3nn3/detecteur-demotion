0177e07068e03dcfdee2effcee24e4233d5725dd
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2020-10-27 14:00:51 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Meh)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+})
+basic.forever(function () {
+	
+})
